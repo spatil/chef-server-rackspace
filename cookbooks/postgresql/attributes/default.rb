04123de9759ecc0fe56bea931a8ea818ac9fb411
Shailesh Patil <shailesh@joshsoftware.com>
2013-09-11 17:48:54 +0530

    added permission for zenith user

--- cookbooks/postgresql/attributes/default.rb
+++ cookbooks/postgresql/attributes/default.rb
@@ -178,6 +178,7 @@ default['postgresql']['pg_hba'] = [
   {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
   {:type => 'local', :db => 'all', :user => 'zenith', :addr => nil, :method => 'md5'},
+  {:type => 'local', :db => 'all', :user => 'zenith', :addr => '10.176.3.58', :method => 'md5'},
 ]
 
 default['postgresql']['password'] = Hash.new
